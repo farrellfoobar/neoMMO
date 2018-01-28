b648196556ba97a77412d5247cf911b91e3f65d8
Patrick <farrellryan@comcast.net>
2018-01-27 17:29:29 -0800

    Added immplementation of basic gameServer

--- src/NeoMMO/gameClient.java
+++ src/NeoMMO/gameClient.java
@@ -1,8 +1,9 @@
+package NeoMMO;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-
 
 public class gameClient 
 {
---
+++ src/NeoMMOserver/gameServer.java
@@ -0,0 +1,32 @@
+package NeoMMOserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class gameServer {
+
+public static void main(String[] args) throws IOException
+{
+        ServerSocket listener = new ServerSocket(9090);
+        Scanner in = new Scanner(System.in);
+
+        Socket socket = listener.accept();
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        String scannerBuffer = "";
+        String temp = "";
+
+        while (true)
+        {
+                while( in.hasNext() )
+                {
+                        out.println( in.next() );
+
+                }
+        }
+}         
+         
+}
