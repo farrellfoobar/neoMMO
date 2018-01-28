4d1f577887e6e8f25f36ee4828b5461fb60bc2ff
Patrick <farrellryan@comcast.net>
2018-01-28 00:21:13 -0800

    debugging

--- src/NeoMMO/gameClient.java
+++ src/NeoMMO/gameClient.java
@@ -85,6 +85,7 @@ class gameMap extends JPanel
 		File temp = new File("");
 		//path should == absolute path + \NeoMMO\assets\PlainGrass.png
 		String path = temp.getAbsolutePath().substring(0, temp.getAbsolutePath().lastIndexOf( containerName )+containerName.length() ) + File.separatorChar + "assets" + File.separatorChar;
+		System.out.println( path );
 		BufferedImage[] imgs = new BufferedImage[images.length];
 		Random rand = new Random();
 		
