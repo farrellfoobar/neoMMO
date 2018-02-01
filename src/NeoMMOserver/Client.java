9c05f967ba113de45afd039596b2ef00d4a8ce38
Patrick <farrellryan@comcast.net>
2018-01-31 16:20:11 -0800

    testing commands

--- src/NeoMMOserver/Client.java
+++ src/NeoMMOserver/Client.java
@@ -70,7 +70,7 @@ public class Client extends Thread
 		System.out.println("trying to find: " + method + " Looking at: ");
 		for( int i = 0; i < methods.length; i++ )
 		{
-			System.out.println(methods[i]);
+			System.out.println(methods[i].getName());
 			if( command.equals(methods[i].getName()) )
 			{
 				try 
